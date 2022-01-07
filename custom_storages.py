63da471128f9b132ffab295eac4a500fa42d264c
suzybee1987 <suzannemiller1987@gmail.com>
2022-01-07 09:02:37 +0000

    Dev: add new AWS for media and static storage

--- custom_storages.py
+++ custom_storages.py
@@ -3,8 +3,14 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
+    """
+    Static storage for AWS
+    """
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """
+    Media storage for AWS
+    """
     location = settings.MEDIAFILES_LOCATION
--- knit_happens/settings.py
+++ knit_happens/settings.py
@@ -181,13 +181,13 @@ if 'USE_AWS' in os.environ:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    #STATIC AND MEDIA FILES - use storage class
+    # STATIC AND MEDIA FILES - use storage class
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
-    #override static and media urls in production
+    # override static and media urls in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
