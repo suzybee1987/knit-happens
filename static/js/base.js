da2bc8ee76e65e52f236178db47eaff0e9cc3338
suzybee1987 <suzannemiller1987@gmail.com>
2022-01-09 10:56:14 +0000

    Style: styling index and base.html footer, navlinks and make responsive

---
+++ static/js/base.js
@@ -0,0 +1,38 @@
+    document.addEventListener('DOMContentLoaded', () => {
+        (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+            const $notification = $delete.parentNode;
+
+            $delete.addEventListener('click', () => {
+                $notification.parentNode.removeChild($notification);
+            });
+        });
+    });
+
+// code from Bulma GitHub to fix the navbar menu always open on mobile 
+// https://github.com/jgthms/bulma/issues/2361
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburgerButton = document.querySelector(".navbar-burger");
+    const navMenu = document.querySelector(".navbar-menu");
+
+    hamburgerButton.addEventListener("click", () => {
+        hamburgerButton.classList.toggle("is-active");
+        navMenu.classList.toggle("is-active");
+    });
+
+    const allDropdowns = document.querySelectorAll(
+        ".navbar-item.has-dropdown"
+    );
+
+    allDropdowns.forEach((dropdown) => {
+        dropdown.addEventListener("click", () => {
+            const elem = dropdown.querySelector(".navbar-dropdown");
+            elem.classList.toggle("is-active");
+        });
+    });
+}); 
+    $(document).ready(function () {
+        // Initialize Uni-Form
+        $(function () {
+            $('form.uniForm').uniform();
+        });
+    });
