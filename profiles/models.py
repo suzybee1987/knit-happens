921bd71988b42eace3b1a5259863cc9cc28c51e9
suzybee1987 <suzannemiller1987@gmail.com>
2022-01-12 13:44:13 +0000

    Dev: comment models.py profiles heroku bug

--- profiles/models.py
+++ profiles/models.py
@@ -12,20 +12,13 @@ class UserProfile(models.Model):
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_phone_number = models.CharField(max_length=20,
-                                            null=True, blank=True)
-    default_street_address1 = models.CharField(max_length=80,
-                                               null=True, blank=True)
-    default_street_address2 = models.CharField(max_length=80,
-                                               null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40,
-                                            null=True, blank=True)
-    default_county = models.CharField(max_length=80,
-                                      null=True, blank=True)
-    default_postcode = models.CharField(max_length=20,
-                                        null=True, blank=True)
-    default_country = CountryField(blank_label='Country',
-                                   null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
 
     def __str__(self):
         return self.user.username
@@ -36,7 +29,7 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     Create or update the user profile
     """
-    if created:
-        UserProfile.objects.create(user=instance)
+    # if created:
+        # UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
-    instance.userprofile.save()
+    # instance.userprofile.save()
