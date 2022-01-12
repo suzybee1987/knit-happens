aceec445882baeaedffd17d035319739674351e3
suzybee1987 <suzannemiller1987@gmail.com>
2022-01-12 09:11:31 +0000

    Dev: amend profiles page and create order page

--- profiles/views.py
+++ profiles/views.py
@@ -16,11 +16,8 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-        else:
-            messages.error(
-                request, 'Update failed. Please ensure the form is valid.')
-    else:
-        form = UserProfileForm(instance=profile)
+
+    form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
