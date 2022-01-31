a469434969929e7d5355e87e59846506d696c303
suzybee1987 <suzannemiller1987@gmail.com>
2022-01-31 12:47:49 +0000

    Test: Test Product Views

--- products/admin.py
+++ products/admin.py
@@ -24,7 +24,7 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
 
-# Reviews and ratings 
+# Reviews
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('review_title', 'review', 'review_author', 'product')
