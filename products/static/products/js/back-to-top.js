7cf789de1054540d5eb5169e0e0c38ecdf7cbd4f
suzybee1987 <suzannemiller1987@gmail.com>
2022-02-02 09:36:20 +0000

    dev: styling checkout and moving js/css to app folders

---
+++ products/static/products/js/back-to-top.js
@@ -0,0 +1,21 @@
+// script from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp 
+    //Get the button
+    let mybutton = document.getElementById("btt-button")
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+        scrollFunction()
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+    mybutton.addEventListener('click', topFunction);
