cbad57e10c36285dcac82370e73bd32c93d770d9
suzybee1987 <suzannemiller1987@gmail.com>
2022-02-02 14:56:56 +0000

    Dev: add privacy policy page

--- home/views.py
+++ home/views.py
@@ -8,3 +8,8 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+    
+def privacy(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/privacy.html')
