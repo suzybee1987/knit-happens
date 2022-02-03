c05aa59b8fb3840ee40a649431a9b7892c6cabba
suzybee1987 <suzannemiller1987@gmail.com>
2022-02-03 08:21:57 +0000

    Dev: migrate to new postgres database

--- knit_happens/settings.py
+++ knit_happens/settings.py
@@ -128,16 +128,10 @@ WSGI_APPLICATION = 'knit_happens.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 
 # Password validation
