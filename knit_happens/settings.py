9fda30585eb6da384d6ccef0532f78f4fdb20a6e
suzybee1987 <suzannemiller1987@gmail.com>
2022-02-03 08:25:54 +0000

    Dev: add database if statement in settings.py

--- knit_happens/settings.py
+++ knit_happens/settings.py
@@ -128,10 +128,16 @@ WSGI_APPLICATION = 'knit_happens.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
 
 # Password validation
