83ff6501693a3275fee81f1b844da6bedcd7c07e
suzybee1987 <suzannemiller1987@gmail.com>
2022-02-07 08:43:03 +0000

    dev: add 403, 404, 500 pages and styling

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="home"),
+    path('', views.index, name="home"),    
     path('privacy/', views.privacy, name="privacy"),
     path('terms/', views.terms, name="terms"),
 ]
